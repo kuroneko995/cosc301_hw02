df24ae8112f899c5462ef5866ac9ce6448c71f4d
Joel Sommers <jsommers@acm.org>
2014-08-30 09:31:56 -0400

    Initial commit

---
+++ hangperson.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <time.h>
+#include <assert.h>
+
+
+/*
+ * Be happy you didn't have to write this function.
+ * Prints a low-tech ASCII gallows.  Max number of misses
+ * is 7 (i.e., if there are 7 misses, the player loses and
+ * the poor sap gets hung).  
+ */
+void print_gallows(int num_missed) {
+    // make sure that we got called in a reasonable way
+    assert(num_missed >= 0 && num_missed <= 7);
+
+    printf("\n\n       |||========|||\n");
+    if (num_missed > 0) {
+        printf ("       |||         |\n");
+    } else {
+        printf ("       |||          \n");
+    }
+
+    if (num_missed > 1) {
+        printf ("       |||         O\n");
+    } else {
+        printf ("       |||          \n");
+    }
+
+    if (num_missed > 2) {
+        if (num_missed > 4) {
+            printf ("       |||        /|\\\n");
+        } else if (num_missed > 3) {
+            printf ("       |||        /| \n");
+        } else {
+            printf ("       |||        /  \n");
+        }
+    } else {
+        printf ("       |||           \n");
+    }
+
+    if (num_missed > 5) {
+        if (num_missed > 6) {
+            printf ("       |||        / \\\n");
+        } else {
+            printf ("       |||        /  \n");
+        }
+    } else {
+        printf ("       |||           \n");
+    }
+
+    printf ("       |||\n");
+    printf ("       |||\n");
+    printf ("     =================\n\n");
+}
+
+/*
+ * Play one game of Hangperson.  The secret word is passed as a
+ * parameter.  The function should return true if the player won,
+ * and false otherwise.
+ */
+bool one_game(const char *word) {
+
+    
+}
+
+
+#ifndef AUTOTEST
+/*
+ * The main function is where everything starts.  Choose a random
+ * word and call one_game.  Feel free to modify the words array,
+ * but don't forget to modify the modulus (currently 4) for choosing 
+ *  a random word from the array.
+ */
+int main() {
+    srandom(time(NULL));
+    char *words[] = { "SUMMER", "AUTUMN", "WINTER", "SPRING" };
+    int word = random() % 4;
+    one_game(words[word]);
+    return 0;
+}
+#endif
